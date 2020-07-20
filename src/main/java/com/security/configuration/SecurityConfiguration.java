f6c6e5263e799366530ab6821f17001e30696032
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-20 16:27:07 +0530

    enabling CSRF

--- src/main/java/com/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/security/configuration/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
+//			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/admin/**").hasAnyRole("ADMIN")
 			.anyRequest().hasAnyRole("USER")
