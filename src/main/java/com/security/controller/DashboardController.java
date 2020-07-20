a2a56207186632f2c83e43e60dee2322efc0a120
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-20 16:50:01 +0530

    logging out and @AuthenticationPrinciple

--- src/main/java/com/security/controller/DashboardController.java
+++ src/main/java/com/security/controller/DashboardController.java
@@ -1,13 +1,19 @@
 package com.security.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.security.entities.User;
 
 @Controller
 public class DashboardController {
 
 	@GetMapping("/dashboard")
-	public String getDashboard() {
+	public String getDashboard(@AuthenticationPrincipal User user, ModelMap modelMap) {
+		System.out.println(user);
+		modelMap.put("user", user);
 		return "dashboard";
 	}
 }
